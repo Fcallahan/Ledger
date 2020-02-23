d5a5474106833567e6633abea73051ffb9602544
Frank Callahan <fcallahan@angelo.edu>
2020-02-23 15:17:13 -0600

    refactored the ledger.jsx to use react hooks and is now a functional component

--- src/components/Ledger.jsx
+++ src/components/Ledger.jsx
@@ -1,72 +1,42 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import LedgerEntry from "./LedgerEntry";
 import { firestore } from "../firebase";
 
-class Ledger extends Component {
-  constructor(props) {
-    super(props);
+const Ledger = ({ user }) => {
+  const [records, setRecords] = useState([]);
 
-    this.state = {
-      entries: [],
-      user: props.user
-    };
-    console.log(this.state.user);
-  }
-
-  componentDidMount() {
-    this.updateUser(this.props.user);
-  }
-
-  updateUser(user) {
-    if (this.state.unsubscribeCallback) {
-      this.state.unsubscribeCallback();
-    }
-
-    var docRef = firestore.collection("ledger").doc(user);
-
-    var unsubscribe = docRef.onSnapshot(doc => {
+  useEffect(() => {
+    (async function update() {
+      var docRef = firestore.collection("ledger").doc(user);
+      let doc = await docRef.get();
       if (doc.exists) {
-        console.log("Document data:", doc.data());
+        //console.log("Document data:", doc.data());
+        console.log("Exists");
         const jsonEntries = doc.data().records.map(e => JSON.parse(e));
-        this.setState({
-          entries: jsonEntries
-        });
+        setRecords(jsonEntries);
       } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
       }
-    });
+    })();
+  });
 
-    this.setState({ unsubscribeCallback: unsubscribe });
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.user !== prevProps.user) {
-      // Check if it's a new user, you can also use some unique property, like the ID  (this.props.user.id !== prevProps.user.id)
-      // this.setState({ user: this.props.user });
-      this.updateUser(this.props.user);
-    }
-  }
-
-  componentWillUnmount() {}
-
-  render() {
-    const entries = this.state.entries.map((trans, idx) => (
-      <LedgerEntry key={idx} {...trans} />
-    ));
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>description</th>
-            <th>amount</th>
-            <th>date</th>
-          </tr>
-        </thead>
-        <tbody>{entries}</tbody>
-      </table>
-    );
-  }
-}
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>description</th>
+          <th>amount</th>
+          <th>date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {records.map((record, idx) => (
+          <LedgerEntry key={idx} {...record} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Ledger;
